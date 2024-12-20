123b8118ce14293e5ebb5fb1dfcb20b4cc9da4de
ecizel <tinhoizel@gmail.com>
2024-12-20 11:58:36 -0300

    passando dark e light mode, organização dos botões para mobile e detalhes de design

--- src/components/ButtonBefore.vue
+++ src/components/ButtonBefore.vue
@@ -6,9 +6,10 @@
 
 <template>
     <a class="
-            py-2 text-sm sm:text-base hover:text-green-500 transition-colors duration-300 relative w-fit
-            before:absolute before:bg-green-500 before:w-0 before:h-px before:bottom-1.5 before:left-1/2
+            py-2 text-sm sm:text-base hover:text-green-400 dark:hover:text-green-500 transition-colors duration-300 relative w-fit
+            before:absolute before:bg-green-400 dark:before:bg-green-500 before:w-0 before:h-px before:bottom-1.5 before:left-1/2
             hover:before:w-full hover:before:left-0 before:transition-all before:duration-300
+            pointer-events-none md:pointer-events-auto
             " 
         v-text="msg"
         :href="path"></a>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,5 @@
 <script setup>
+
     import { ref } from 'vue'
     import ButtonBefore from './ButtonBefore.vue'
     import CloseButton from './CloseButton.vue'
@@ -10,7 +11,7 @@
 <template>
     <!-- navbar -->
     <div 
-        class="sticky flex w-screen sm:w-[90vw] sm:h-10 sm:rounded-b-lg place-self-center bg-slate-900/70 backdrop-blur-[1px] sm:border-b border-green-500 px-5 sm:px-8 justify-between items-center">
+        class="sticky flex w-screen sm:w-[90vw] sm:h-10 sm:rounded-b-lg place-self-center bg-slate-200/70 dark:bg-slate-900/70 backdrop-blur-[1px] sm:border-b-2 border-green-400 dark:border-green-500 px-5 sm:px-8 justify-between items-center">
         <ButtonBefore msg="Task-app" path="#"/>
         <div class="hidden sm:flex gap-8 sm:gap-[10vw]">
             <ButtonBefore msg="Home" path="#"/>
@@ -22,11 +23,10 @@
     </div>
 
     <!-- dropbar -->
-    <div class="sticky sm:hidden px-5 flex flex-col bg-slate-900/70 border-b border-b-green-500 overflow-auto transition-all duration-300"
+    <div class="absolute w-full sm:hidden px-5 flex flex-col bg-slate-200/70 dark:bg-slate-900/70 border-b border-b-green-500 overflow-hidden transition-all duration-300"
          :class="dropbarOn ? 'h-20' : 'h-0'">
         <ButtonBefore msg="Home" path="#"/>
         <ButtonBefore msg="Other projects" path="#"/>
     </div>
-
     
 </template>
